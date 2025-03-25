c7b8cbe17b9cad25e816004c5610578d9fa10fdb
WAluiGIII Dulaugon <ldulaugon123@gmail.com>
2025-03-25 16:57:53 +0800

    Now has a repository/database that is connected to localhost

--- StarBuko/Presenters/MainPresenter.cs
+++ StarBuko/Presenters/MainPresenter.cs
@@ -7,31 +7,36 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using StarBuko.Models;
 using StarBuko.Views;
+using StarBuko.Repositories;
 
 namespace StarBuko.Presenters
 {
     public class MainPresenter
     {
         private IMainForm _view;
-        private List<Product> _products;
+        //private List<Product> _products;
         private BindingList<LineItem> _lineItems;
+        ProductRepository _repository;
+        List<Product> _products;
 
         public MainPresenter(IMainForm view)
         {
             _view = view;
             _lineItems = new BindingList<LineItem>();
+            _repository = new ProductRepository();
+            _products = _repository.GetProducts();
 
-            _products = new List<Product>
-            {
-                new Product { Name = "Creamy Pure Matcha Latte", Price = 180.00m, ImagePath = "Images/matcha_latte.jpeg" },
-                new Product { Name = "XOXO Frappuccino", Price = 150.00m, ImagePath = "Images/xoxo_frappucino.jpeg" },
-                new Product { Name = "Strawberry Açaí with Lemonade", Price = 160.00m, ImagePath = "Images/strawberry_acai.jpeg" },
-                new Product { Name = "Pink Drink with Strawberry Açaí", Price = 165.00m, ImagePath = "Images/pink_drink.jpeg" },
-                new Product { Name = "Dragon Drink with Mango Dragonfruit ", Price = 170.00m, ImagePath = "Images/dragon_drink.jpeg" },
-                new Product { Name = "Strawberries & Cream Frappuccino", Price = 175.00m, ImagePath = "Images/strawberries_cream.jpg" },
-                new Product { Name = "Chocolate Chip Cream Frappuccino", Price = 180.00m, ImagePath = "Images/chocolate_chip.jpg" },
-                new Product { Name = "Dark Caramel Coffee Frappuccino", Price = 170.00m, ImagePath = "Images/dark_caramel_frappucino.jpg" },
-            };
+            //_products = new List<Product>
+            //{
+            //    new Product { Name = "Creamy Pure Matcha Latte", Price = 180.00m, ImagePath = "Images/matcha_latte.jpeg" },
+            //    new Product { Name = "XOXO Frappuccino", Price = 150.00m, ImagePath = "Images/xoxo_frappucino.jpeg" },
+            //    new Product { Name = "Strawberry Açaí with Lemonade", Price = 160.00m, ImagePath = "Images/strawberry_acai.jpeg" },
+            //    new Product { Name = "Pink Drink with Strawberry Açaí", Price = 165.00m, ImagePath = "Images/pink_drink.jpeg" },
+            //    new Product { Name = "Dragon Drink with Mango Dragonfruit ", Price = 170.00m, ImagePath = "Images/dragon_drink.jpeg" },
+            //    new Product { Name = "Strawberries & Cream Frappuccino", Price = 175.00m, ImagePath = "Images/strawberries_cream.jpg" },
+            //    new Product { Name = "Chocolate Chip Cream Frappuccino", Price = 180.00m, ImagePath = "Images/chocolate_chip.jpg" },
+            //    new Product { Name = "Dark Caramel Coffee Frappuccino", Price = 170.00m, ImagePath = "Images/dark_caramel_frappucino.jpg" },
+            //};
 
             _view.DisplayProducts(_products);
             _view.DisplayLineItems(_lineItems);
---
+++ StarBuko/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using StarBuko.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using MySqlConnector;
+
+namespace StarBuko.Repositories
+{
+    class ProductRepository
+    {
+        string _connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString; 
+
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>(); 
+            //MessageBox.Show(_connectionString)
+            using (MySqlConnection conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                String query = "SELECT * FROM products";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            products.Add(new Product
+                            {
+                                Name = reader.GetString("productName"),
+                                Price = reader.GetDecimal("price"),
+                                ImagePath = reader.GetString("image")
+                            }); ;
+                        }
+                    }
+                }
+            }
+            return products; 
+        }
+
+    }
+}
